b404cc036d9368621f215cf132a58633da17fbbd
wwwouaiebe <github.christian@guyette.be>
2021-01-10 15:21:35 +0100

    Issue #148 ongoing

--- src/core/GeoCoder.js
+++ src/core/GeoCoder.js
@@ -30,6 +30,7 @@ Changes:
 		- Issue #120 : Review the UserInterface
 	- v2.0.0:
 		- Issue #138 : Protect the app - control html entries done by user.
+		- Issue #148 : Nominatim gives bad responses for cities... find a temporary solution.
 Doc reviewed 20200802
 Tests ...
 
@@ -70,8 +71,9 @@ Tests ...
 
 import { theHttpRequestBuilder } from '../util/HttpRequestBuilder.js';
 import { theConfig } from '../data/Config.js';
-import { ZERO, ONE } from '../util/Constants.js';
+import { ZERO, ONE, TWO } from '../util/Constants.js';
 import { theHTMLSanitizer } from '../util/HTMLSanitizer.js';
+import { theGeometry } from '../util/Geometry.js';
 
 /**
 @------------------------------------------------------------------------------------------------------------------------------
@@ -84,7 +86,160 @@ import { theHTMLSanitizer } from '../util/HTMLSanitizer.js';
 @------------------------------------------------------------------------------------------------------------------------------
 */
 
+const ourQueryDistance = Math.max (
+	theConfig.note.svgHamletDistance,
+	theConfig.note.svgVillageDistance,
+	theConfig.note.svgCityDistance,
+	theConfig.note.svgTownDistance
+);
+
 function ourNewGeoCoder ( ) {
+
+	let myLatLng = null;
+
+	/**
+	@--------------------------------------------------------------------------------------------------------------------------
+
+	@function myParseOverpassData
+	@desc This function parse the overpass data, seraching a city name and a place name in the data.
+	City name is the osm area name with the greater admin_level (but smaller than 9 ).
+	Place name is the osm area name with the greater admin_level (but greater than 8 ) or the nearest node
+	with a place tag and values of hamlet, village, city or town.
+	@return {Object} an object with the city, place and country foud
+	@private
+
+	@--------------------------------------------------------------------------------------------------------------------------
+	*/
+
+	function myParseOverpassData ( overpassData ) {
+		const OSM_CITY_ADMIN_LEVEL = '8';
+		const OSM_COUNTRY_ADMIN_LEVEL = '2';
+		const LNG = theConfig.nominatim.language;
+		let adminNames = [];
+		let places = {
+			hamlet : {
+				name : null,
+				distance : Number.MAX_VALUE,
+				maxDistance : theConfig.note.svgHamletDistance
+			},
+			village : {
+				name : null,
+				distance : Number.MAX_VALUE,
+				maxDistance : theConfig.note.svgVillageDistance
+			},
+			city : {
+				name : null,
+				distance : Number.MAX_VALUE,
+				maxDistance : theConfig.note.svgCityDistance
+			},
+			town : {
+				name : null,
+				distance : Number.MAX_VALUE,
+				maxDistance : theConfig.note.svgTownDistance
+			}
+		};
+		overpassData.elements.forEach (
+			element => {
+				if ( 'area' === element.type ) {
+					let elementName = element.tags.name;
+					if ( LNG && '*' !== LNG && element.tags [ 'name:' + LNG ] ) {
+						elementName = element.tags [ 'name:' + LNG ];
+					}
+					adminNames [ Number.parseInt ( element.tags.admin_level ) ] = elementName;
+				}
+				if (
+					'node' === element.type &&
+					element.tags &&
+					element.tags.place &&
+					places [ element.tags.place ] &&
+					element.tags.name
+				) {
+					let nodeDistance = theGeometry.pointsDistance ( myLatLng, [ element.lat, element.lon ] );
+					let place = places [ element.tags.place ];
+					if ( place.maxDistance > nodeDistance && place.distance > nodeDistance ) {
+						place.distance = nodeDistance;
+						place.name = element.tags.name;
+					}
+				}
+			}
+		);
+
+		let adminCity = null;
+		let adminHamlet = null;
+
+		for ( let namesCounter = TWO; namesCounter < adminNames.length; namesCounter ++ ) {
+			if ( 'undefined' !== typeof ( adminNames [ namesCounter ] ) ) {
+				if ( OSM_CITY_ADMIN_LEVEL >= namesCounter ) {
+					adminCity = adminNames [ namesCounter ];
+				}
+				else {
+					adminHamlet = adminNames [ namesCounter ];
+				}
+			}
+		}
+
+		let placeName = null;
+		let placeDistance = Number.MAX_VALUE;
+
+		Object.values ( places ).forEach (
+			place => {
+				if ( place.distance < placeDistance ) {
+					placeDistance = place.distance;
+					placeName = place.name;
+				}
+			}
+		);
+
+		placeName = adminHamlet || placeName;
+		if ( placeName === adminCity ) {
+			placeName = null;
+		}
+
+		return {
+			city : adminCity,
+			place : adminHamlet || placeName,
+			country : adminNames [ OSM_COUNTRY_ADMIN_LEVEL ]
+		};
+	}
+
+	/**
+	@--------------------------------------------------------------------------------------------------------------------------
+
+	@function myParseNominatimData
+	@desc This function parse the nominatim data, seraching a street name, a house number and a name from osm data
+	@return {Object} an object with the street, name and country found.
+	@private
+
+	@--------------------------------------------------------------------------------------------------------------------------
+	*/
+
+	function myParseNominatimData ( nominatimData ) {
+		let street = '';
+		if ( ! nominatimData.error ) {
+
+			// street
+			if ( nominatimData.address.house_number ) {
+				street += nominatimData.address.house_number + ' ';
+			}
+			if ( nominatimData.address.road ) {
+				street += nominatimData.address.road + ' ';
+			}
+			else if ( nominatimData.address.pedestrian ) {
+				street += nominatimData.address.pedestrian + ' ';
+			}
+
+			return {
+				street : street,
+				nameDetails : nominatimData.namedetails.name,
+				country : nominatimData.address.country
+			};
+		}
+		return {
+			street : null,
+			nameDetails : null,
+			country : null
+		};
+	}
 
 	/**
 	@--------------------------------------------------------------------------------------------------------------------------
@@ -100,54 +255,35 @@ function ourNewGeoCoder ( ) {
 	class GeoCoder {
 
 		/**
-		Parse the Nominatim response
-		@param {Object} geoCoderData the response from Nominatim
+		Parse the Nominatim and overpassAPI response
+		@param {Array.<object>} data the responses from Nominatim and OverpassAPI
 		@return {GeoCoderAddress} the name and address of the point
 		*/
 
-		parseResponse ( geoCoderData ) {
-			let street = '';
-			let namedetails = '';
-			let city = '';
-			if ( ! geoCoderData.error ) {
+		parseResponse ( data ) {
+			let overpassData = myParseOverpassData ( data [ ONE ].value );
+			let nominatimData = myParseNominatimData ( data [ ZERO ].value );
 
-				// street
-				if ( geoCoderData.address.house_number ) {
-					street += geoCoderData.address.house_number + ' ';
-				}
-				if ( geoCoderData.address.road ) {
-					street += geoCoderData.address.road + ' ';
-				}
-				else if ( geoCoderData.address.pedestrian ) {
-					street += geoCoderData.address.pedestrian + ' ';
-				}
+			let city = overpassData.city;
+			if ( overpassData.place ) {
+				city += ' (' + overpassData.place + ')';
+			}
+			let street = nominatimData.street;
 
-				// city
-				if ( geoCoderData.address.village ) {
-					city = geoCoderData.address.village;
-				}
-				else if ( geoCoderData.address.town ) {
-					city = geoCoderData.address.town;
-				}
-				else if ( geoCoderData.address.city ) {
-					city = geoCoderData.address.city;
-				}
+			if ( ! street && ! city ) {
+				city = nominatimData.country || overpassData.country || '';
+			}
 
-				// country
-				if ( '' === street && '' === city ) {
-					street = geoCoderData.address.country;
-				}
+			street = street || '';
 
-				// name
-				namedetails = geoCoderData.namedetails.name || '';
-				if ( street.includes ( namedetails ) || city.includes ( namedetails ) ) {
-					namedetails = '';
-				}
+			let nameDetails = nominatimData.nameDetails || '';
+			if ( street.includes ( nameDetails ) || city.includes ( nameDetails ) ) {
+				nameDetails = '';
 			}
 
 			return Object.seal (
 				{
-					name : theHTMLSanitizer.sanitizeToJsString ( namedetails ),
+					name : theHTMLSanitizer.sanitizeToJsString ( nameDetails ),
 					street : theHTMLSanitizer.sanitizeToJsString ( street ),
 					city : theHTMLSanitizer.sanitizeToJsString ( city )
 				}
@@ -161,6 +297,7 @@ function ourNewGeoCoder ( ) {
 		*/
 
 		getPromiseAddress ( latLng ) {
+			myLatLng = latLng;
 			let NominatimUrl =
 				theConfig.nominatim.url + 'reverse?format=json&lat=' +
 				latLng [ ZERO ] + '&lon=' + latLng [ ONE ] +
@@ -175,7 +312,29 @@ function ourNewGeoCoder ( ) {
 				requestHeaders = [ { headerName : 'accept-language', headerValue : '' } ];
 			}
 
-			return theHttpRequestBuilder.getJsonPromise ( NominatimUrl, requestHeaders );
+			/*
+			https://lz4.overpass-api.de/api/interpreter?
+				data=[out:json][timeout:40];
+				is_in(50.644242,5.572354)->.e;
+				area.e[admin_level][boundary="administrative"]->.f;
+				node(around:1500,50.644242,5.572354)[place]->.g;
+				(.f;.g;)->.h;
+				.h out;
+			*/
+
+			let overpassAPIUrl = theConfig.overpassApi.url +
+				'?data=[out:json][timeout:' +
+				theConfig.note.svgTimeOut + '];is_in(' + latLng [ ZERO ] + ',' + latLng [ ONE ] +
+				')->.e;area.e[admin_level][boundary="administrative"]->.f;node(around:' +
+				ourQueryDistance + ',' + latLng [ ZERO ] + ',' + latLng [ ONE ] +
+				')[place]->.g;(.f;.g;)->.h;.h out;';
+
+			return Promise.allSettled (
+				[
+					theHttpRequestBuilder.getJsonPromise ( NominatimUrl, requestHeaders ),
+					theHttpRequestBuilder.getJsonPromise ( overpassAPIUrl )
+				]
+			);
 		}
 	}
 
