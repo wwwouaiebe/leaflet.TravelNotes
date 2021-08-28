eb418f18e0b23bbb35f73bb16b945376cc91de31
wwwouaiebe <github.christian@guyette.be>
2021-08-28 07:37:36 +0200

    Issue #175 ongoing

--- src/routeProviders/PublicTransportRouteProvider.js
+++ src/routeProviders/PublicTransportRouteProvider.js
@@ -19,7 +19,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Changes:
 	- v2.1.0:
 		- Issue ♯150 : Merge travelNotes and plugins
-Doc reviewed ...
+	- v3.0.0:
+		- Issue ♯175 : Private and static fields and methods are coming
+Doc reviewed 20210827
 Tests ...
 */
 
@@ -46,162 +48,7 @@ Tests ...
 import { ZERO, ONE, LAT_LNG, HTTP_STATUS_OK } from '../util/Constants.js';
 import SelectDialog from '../dialogs/SelectDialog.js';
 import PublicTransportRouteBuilder from '../routeProviders/PublicTransportRouteBuilder.js';
-
-
-let ourUserLanguage = 'fr';
-let ourRoute = null;
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourParseResponse
-@desc coming soon...
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourParseResponse ( response, onOk, onError ) {
-	let publicTransportRouteBuilder = new PublicTransportRouteBuilder ( ourRoute );
-	publicTransportRouteBuilder.buildRoute ( response, onOk, onError );
-}
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourGetRelationsUrl
-@desc coming soon...
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourGetRelationsUrl ( ) {
-	return window.TaN.overpassApiUrl +
-		'?data=[out:json];node["public_transport"="stop_position"]["train"="yes"](around:400.0,' +
-		ourRoute.wayPoints.first.lat.toFixed ( LAT_LNG.fixed ) +
-		',' +
-		ourRoute.wayPoints.first.lng.toFixed ( LAT_LNG.fixed ) +
-		')->.s;rel(bn.s)->.s;node["public_transport"="stop_position"]["train"="yes"](around:400.0,' +
-		ourRoute.wayPoints.last.lat.toFixed ( LAT_LNG.fixed ) +
-		',' +
-		ourRoute.wayPoints.last.lng.toFixed ( LAT_LNG.fixed ) +
-		')->.e;rel(bn.e)->.e;rel.e.s;out tags;';
-}
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourGetWayNodesUrl
-@desc coming soon...
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourGetWayNodesUrl ( relationId ) {
-	return window.TaN.overpassApiUrl + '?data=[out:json];rel(' +
-		relationId.toFixed ( ZERO ) +
-		');way(r)->.e;way.e["railway"="rail"];(._;>;rel(' +
-		relationId.toFixed ( ZERO ) +
-		'););out;';
-}
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourGetDialogPromise
-@desc coming soon...
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourGetDialogPromise ( response ) {
-
-	if ( ZERO === response.elements.length ) {
-		return Promise.reject ( new Error ( 'No relations found' ) );
-	}
-
-	let selectOptionsData = [];
-	response.elements.forEach (
-		dataElement => {
-			selectOptionsData.push ( { text : dataElement.tags.name, objId : dataElement.id } );
-		}
-	);
-
-	let selectDialog = new SelectDialog (
-		{
-			title : 'Relations',
-			text : 'select a relation : ',
-			selectOptionsData : selectOptionsData
-		}
-	);
-
-	// baseDialog.show ( ) return a Promise...
-	return selectDialog.show ( );
-
-}
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourGetRoute
-@desc call the provider, wait for the response and then parse the provider response. Notice that we have two calls to the
-Provider: one for the relation list and one for the ways and nodes. Notice also the dialog box between the 2 calls.
-@param {function} onOk a function to pass to the ourParseResponse
-@param {function} onError a function to pass to ourParseResponse or to call when an error occurs
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourGetRoute ( onOk, onError ) {
-
-	fetch ( ourGetRelationsUrl ( ) )
-		.then (
-			responseRelations => {
-				if ( HTTP_STATUS_OK === responseRelations.status && responseRelations.ok ) {
-					responseRelations.json ( )
-						.then ( ourGetDialogPromise )
-						.then ( relationId => fetch ( ourGetWayNodesUrl ( relationId ) ) )
-						.then (
-							responseWayNodes => {
-								if ( HTTP_STATUS_OK === responseWayNodes.status && responseWayNodes.ok ) {
-									responseWayNodes.json ( )
-										.then ( wayNodes => ourParseResponse ( wayNodes, onOk, onError ) );
-								}
-								else {
-									onError ( new Error ( 'An error occurs...' ) );
-								}
-							}
-						)
-						.catch ( ( ) => onError ( new Error ( 'An error occurs...' ) ) );
-				}
-				else {
-					onError ( new Error ( 'An error occurs...' ) );
-				}
-			}
-		);
-}
-
-/**
-@------------------------------------------------------------------------------------------------------------------------------
-
-@function ourGetPromiseRoute
-@desc call the provider, wait for the response and then parse the provider response into the route itinerary object
-@param {route} route a Route object with at least two WayPoints completed
-@return a Promise completed with a function that call the provider, wait the response and then will parse the response
-in the route itinerary
-@private
-
-@------------------------------------------------------------------------------------------------------------------------------
-*/
-
-function ourGetPromiseRoute ( route ) {
-	ourRoute = route;
-	return new Promise ( ourGetRoute );
-}
+import BaseRouteProvider from '../routeProviders/BaseRouteProvider.js';
 
 /**
 @------------------------------------------------------------------------------------------------------------------------------
@@ -216,13 +63,130 @@ of Providers of TravelNotes
 @------------------------------------------------------------------------------------------------------------------------------
 */
 
-class PublicTransportRouteProvider {
+class PublicTransportRouteProvider extends BaseRouteProvider {
 
-	constructor ( ) {
-		Object.freeze ( this );
+	#userLanguage = 'fr';
+
+	/**
+	The provider key. Will be set by TravelNotes
+	@private
+	*/
+
+	#providerKey = '';
+
+	/**
+	A reference to the edited route
+	*/
+
+	#route = null;
+
+	/**
+	@private
+	*/
+
+	#parseResponse ( waysNodes, onOk, onError ) {
+		let publicTransportRouteBuilder = new PublicTransportRouteBuilder ( this.#route );
+		publicTransportRouteBuilder.buildRoute ( waysNodes, onOk, onError );
 	}
 
-	getPromiseRoute ( route ) { return ourGetPromiseRoute ( route ); }
+	/**
+	@private
+	*/
+
+	#getWaysNodesUrl ( relationId ) {
+		return window.TaN.overpassApiUrl + '?data=[out:json];rel(' +
+			relationId.toFixed ( ZERO ) +
+			');way(r)->.e;way.e["railway"="rail"];(._;>;rel(' +
+			relationId.toFixed ( ZERO ) +
+			'););out;';
+	}
+
+	/**
+	@private
+	*/
+
+	#getDialogPromise ( relations ) {
+
+		if ( ZERO === relations.elements.length ) {
+			return Promise.reject ( new Error ( 'No relations found' ) );
+		}
+
+		let selectOptionsData = [];
+		relations.elements.forEach (
+			relationElement => {
+				selectOptionsData.push ( { text : relationElement.tags.name, objId : relationElement.id } );
+			}
+		);
+
+		let selectDialog = new SelectDialog (
+			{
+				title : 'Relations',
+				text : 'select a relation : ',
+				selectOptionsData : selectOptionsData
+			}
+		);
+
+		// baseDialog.show ( ) return a Promise...
+		return selectDialog.show ( );
+
+	}
+
+	/**
+	@private
+	*/
+
+	#getRelationsUrl ( ) {
+		return window.TaN.overpassApiUrl +
+			'?data=[out:json];node["public_transport"="stop_position"]["train"="yes"](around:400.0,' +
+			this.#route.wayPoints.first.lat.toFixed ( LAT_LNG.fixed ) +
+			',' +
+			this.#route.wayPoints.first.lng.toFixed ( LAT_LNG.fixed ) +
+			')->.s;rel(bn.s)->.s;node["public_transport"="stop_position"]["train"="yes"](around:400.0,' +
+			this.#route.wayPoints.last.lat.toFixed ( LAT_LNG.fixed ) +
+			',' +
+			this.#route.wayPoints.last.lng.toFixed ( LAT_LNG.fixed ) +
+			')->.e;rel(bn.e)->.e;rel.e.s;out tags;';
+	}
+
+	/**
+	call the provider, wait for the response and then parse the provider response. Notice that we have two calls to the
+	Provider: one for the relation list and one for the ways and nodes. Notice also the dialog box between the 2 calls.
+	@param {function} onOk a function to pass to the ourParseResponse
+	@param {function} onError a function to pass to ourParseResponse or to call when an error occurs
+	@private
+	*/
+
+	#getRoute ( onOk, onError ) {
+		fetch ( this.#getRelationsUrl ( ) )
+			.then (
+				responseRelations => {
+					if ( HTTP_STATUS_OK === responseRelations.status && responseRelations.ok ) {
+						responseRelations.json ( )
+							.then ( relations => this.#getDialogPromise ( relations ) )
+							.then ( relationId => fetch ( this.#getWaysNodesUrl ( relationId ) ) )
+							.then (
+								responseWaysNodes => {
+									if ( HTTP_STATUS_OK === responseWaysNodes.status && responseWaysNodes.ok ) {
+										responseWaysNodes.json ( )
+											.then ( waysNodes => this.#parseResponse ( waysNodes, onOk, onError ) );
+									}
+									else {
+										onError ( new Error ( 'An error occurs...' ) );
+									}
+								}
+							)
+							.catch ( ( ) => onError ( new Error ( 'An error occurs...' ) ) );
+					}
+					else {
+						onError ( new Error ( 'An error occurs...' ) );
+					}
+				}
+			);
+	}
+
+	constructor ( ) {
+		super ( );
+	}
 
 	get icon ( ) {
 		return 'data:image/svg+xml;utf8,' +
@@ -230,7 +194,11 @@ class PublicTransportRouteProvider {
 			'<path d="M 5,0 C 3.025911,-0.0084 1,3 1,7 l 0,2 c 0,1 1,2 2,2 l 8,0 c 1,0 2,-1 2,-2 L 13,7 C 13,3 11,0 9,0 z m ' +
 			'-1,3 6,0 c 0,0 1,1 1,3 L 3.03125,6 C 2.994661,3.9916 4,3 4,3 z M 3,8 6,8 6,9 3,9 z m 5,0 3,0 0,1 -3,0 z m -6,4 ' +
 			'-1,2 3,0 1,-2 z m 7,0 1,2 3,0 -1,-2 z"/></g></svg>';
+	}
 
+	getPromiseRoute ( route ) {
+		this.#route = route;
+		return new Promise ( ( onOk, onError ) => this.#getRoute ( onOk, onError ) );
 	}
 
 	get name ( ) { return 'PublicTransport'; }
@@ -242,10 +210,11 @@ class PublicTransportRouteProvider {
 	get providerKeyNeeded ( ) { return false; }
 
 	get providerKey ( ) { return ONE; }
-	set providerKey ( ProviderKey ) { }
+	set providerKey ( providerKey ) { }
 
-	get userLanguage ( ) { return ourUserLanguage; }
-	set userLanguage ( UserLanguage ) { ourUserLanguage = UserLanguage; }
+	get userLanguage ( ) { return this.#userLanguage; }
+	set userLanguage ( userLanguage ) { this.#userLanguage = userLanguage; }
+
 }
 
 window.TaN.addProvider ( new PublicTransportRouteProvider ( ) );
